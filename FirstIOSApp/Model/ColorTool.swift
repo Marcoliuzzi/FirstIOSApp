70dddfd8a1f5b4fef6aa79723e3b45740e8c07fc
Marcoliuzzi <43071141+Marcoliuzzi@users.noreply.github.com>
2018-10-09 08:44:28 -0600

    Created model and added a file

---
+++ FirstIOSApp/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSApp
+//
+//  Created by Liuzzi, Marco on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
